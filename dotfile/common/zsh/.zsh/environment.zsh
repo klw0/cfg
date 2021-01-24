7a769d01840534aa97b80624ba042916f832d699
Keith Whitney <keithwhitney0@gmail.com>
2021-01-24 18:07:45 -0500

    zsh: Allow comments in interactive shells

--- dotfile/common/zsh/.zsh/environment.zsh
+++ dotfile/common/zsh/.zsh/environment.zsh
@@ -11,6 +11,7 @@ setopt BRACE_CCL          # Allow brace character class list expansion.
 setopt COMBINING_CHARS    # Combine zero-length punctuation characters (accents)
                           # with the base character.
 setopt RC_QUOTES          # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
+setopt INTERACTIVE_COMMENTS # Allow comments in interactive shells
 
 # Jobs
 setopt LONG_LIST_JOBS     # List jobs in the long format by default.
